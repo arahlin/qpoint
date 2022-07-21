e810bccfc2294272e64f7d0f4557d085a433fad1
Sasha Rahlin <arahlin@fnal.gov>
2022-07-21 15:23:07 -0500

    remove unnecessary build classes, use setuptools

--- setup.py
+++ setup.py
@@ -4,57 +4,37 @@ from __future__ import print_function
 
 # numpy.distutils handles intel compiler
 # need to pass --compiler=intel (or add to [build_ext] section of setup.cfg)
-from numpy.distutils.core import setup, Extension
-from numpy.distutils.command.build import build
-from numpy.distutils.command.build_ext import build_ext
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
 
-try:
-    import sphinx.setup_command
-
-    sphinx_found = True
-except ImportError:
-    sphinx_found = False
 import os, glob
 import sys
-import sysconfig
-
-# get version from git tag
 import subprocess as sp
 
+# get version from git tag
 version = os.getenv("QPOINT_VERSION")
 if version is None:
     cmd = 'git describe --abbrev=4 --dirty --always --tags'.split()
     version = sp.check_output(cmd).strip().decode()
 version_simple = version.split('-')[0]
-vtup = tuple(int(x) for x in version_simple.split('.'))
+
+# write version file
+with open('python/_version.py', 'w') as f:
+    vtup = tuple(int(x) for x in version_simple.split('.'))
+    f.write('__version__ = {:r}\n'.format(version_simple))
+
 print('qpoint version {}'.format(version_simple))
 varg = '-DQP_VERSION=\"{}\"'.format(version)
 
-# build directory names
-def build_dir_name(name):
-    path = 'build/{name}.{platform}-{version[0]}.{version[1]}'.format(
-        name=name, platform=sysconfig.get_platform(), version=sys.version_info
+# build dependencies
+if not os.path.exists("src/qp_iers_bulletin_a.c"):
+    print('make qp_iers_bulletin_a.c')
+    sp.check_call(
+        'cd src && {} make_iers_bulletin_a_dot_c.py'.format(sys.executable),
+        shell=True,
     )
-    return os.path.abspath(path)
-
 
 # build classes
-class BuildLib(build):
-    def run(self):
-        # build dependencies
-        print('make -C src qp_iers_bulletin_a.c')
-        sp.check_call(
-            'PYTHON={} make -C src qp_iers_bulletin_a.c'.format(sys.executable),
-            shell=True,
-        )
-
-        # write version files
-        with open('python/_version.py', 'w') as f:
-            f.write('__version__ = ({:d}, {:d}, {:d})\n'.format(*vtup))
-
-        build.run(self)
-
-
 class BuildExt(build_ext):
     def build_extensions(self):
         c = self.compiler.compiler[0]
@@ -67,22 +47,6 @@ class BuildExt(build_ext):
             for e in self.extensions:
                 e.extra_compile_args.append('-qopenmp')
         build_ext.build_extensions(self)
-
-
-if sphinx_found:
-
-    class BuildDoc(sphinx.setup_command.BuildDoc):
-        def run(self):
-            sys.path.insert(0, build_dir_name('lib'))
-            sphinx.setup_command.BuildDoc.run(self)
-
-else:
-
-    class BuildDoc(build):
-        user_options = [('all', 'a', '')]
-
-        def initialize_options(self):
-            raise ImportError('sphinx not found, cannot build docs')
 
 
 # setup extension arguments
@@ -103,26 +67,14 @@ ext_qp = Extension(
 # run setup
 setup(
     name='qpoint',
-    version=version,
+    version=version_simple,
     description='A lightweight quaternion-based library for efficient telescope pointing.',
     author='Alexandra Rahlin',
     packages=['qpoint'],
     package_dir={'qpoint': 'python'},
     ext_modules=[ext_qp],
     cmdclass={
-        'build': BuildLib,
-        'build_src': BuildLib,
         'build_ext': BuildExt,
-        'build_sphinx': BuildDoc,
     },
     install_requires=['numpy>=1.10.0'],
-    command_options={
-        'build_sphinx': {
-            'project': ('setup.py', 'qpoint'),
-            'version': ('setup.py', version_simple),
-            'release': ('setup.py', version_simple),
-            'source_dir': ('setup.py', 'docs'),
-            'build_dir': ('setup.py', build_dir_name('docs')),
-        }
-    },
 )
